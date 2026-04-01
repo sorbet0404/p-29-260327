197f6b73de5ab05c48f97ac53c1964bc335f1383
sorbet0404 <cyclebae@naver.com>
2026-04-01 11:30:24 +0900

    인증 방식 2개로 늘리기

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -9,42 +9,37 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 @Component
-//생명주기는 request수준으로, 요청이 있을때마다 새로운 객체가 생성되고 끝나면 소멸.
 @RequiredArgsConstructor
 public class Rq {
-    private final HttpServletRequest request;//얘가 사실 requestscope역할 해준다.
-    private final MemberService memberService;
+
+    private final HttpServletRequest request;
     private final HttpServletResponse response;
-
-
+    private final MemberService memberService;
 
     public Member getActor() {
-        String apiKey = null;
-        //얘는 어떻게 넘어온거?
-        String authorizationHeader = request.getHeader("Authorization");
 
-        //헤더 방식일때.
-        if(authorizationHeader != null && !authorizationHeader.isEmpty()) {
+        String authorizationHeader = getHeader("Authorization", "");
 
+        String apiKey;
+        String accessToken;
+
+        if (authorizationHeader != null) {
+            // 헤더 방식
             if (!authorizationHeader.startsWith("Bearer ")) {
-                throw new ServiceException("401-2", "헤더의 인증 정보 형식이 올바르지 않습니다.");
-            }
-            apiKey = authorizationHeader.replace("Bearer ", "");
-        } else {//쿠키 방식일때.
-            //여러개가 들어올수 있으니까 배열로 받기
-            Cookie[] cookies = request.getCookies();
-
-            if(cookies == null) {
-                throw new ServiceException("401-1", "인증 정보가 없습니다.");
+                throw new ServiceException("401-2", "잘못된 형식의 인증데이터입니다.");
             }
 
-            for(Cookie cookie : cookies) {
-                if(cookie.getName().equals("apiKey")) {
-                    apiKey = cookie.getValue();
-                    break;
-                }
-            }
+            String[] headerAuthorizationBits = authorizationHeader.split(" ", 3);            apiKey = authorizationHeader.replace("Bearer ", "");
+
+            apiKey = headerAuthorizationBits[1];
+            accessToken = headerAuthorizationBits.length == 3 ? headerAuthorizationBits[2] : "";
+        } else {
+            apiKey = getCookieValue("apiKey", "");
+            accessToken = getCookieValue("accessToken", "");
         }
 
         if(apiKey.isBlank()) {
@@ -55,17 +50,28 @@ public class Rq {
                 () -> new ServiceException("401-1", "유효하지 않은 API 키입니다.")
         );
     }
-    public void addCookie(String name, String value) {
 
-        Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/");//사용할수 있는 경로 설정
-        cookie.setHttpOnly(true);//JS에서 가져갈수 있는지 설정
-        cookie.setDomain("localhost");//사용할수 있는 도메인 설정
-
-        response.addCookie(
-                cookie
-        );
+    private String getHeader(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getHeader(name))
+                .filter(headerValue -> !headerValue.isBlank())
+                .orElse(defaultValue);
     }
+
+    private String getCookieValue(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getCookies())
+                .flatMap(
+                        cookies ->
+                                Arrays.stream(cookies)
+                                        .filter(cookie -> cookie.getName().equals(name))
+                                        .map(Cookie::getValue)
+                                        .filter(value -> !value.isBlank())
+                                        .findFirst()
+                )
+                .orElse(defaultValue);
+    }
+
     public void deleteCookie(String name) {
         Cookie cookie = new Cookie(name, "");
         cookie.setPath("/");
@@ -74,5 +80,17 @@ public class Rq {
         cookie.setMaxAge(0);
 
         response.addCookie(cookie);
+    }
+
+    public void addCookie(String name, String value) {
+
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setDomain("localhost");
+
+        response.addCookie(
+                cookie
+        );
     }
 }
