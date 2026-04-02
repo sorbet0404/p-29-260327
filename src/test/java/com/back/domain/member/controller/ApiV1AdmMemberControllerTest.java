0f39ab8ca633b76c86846fadd286ea2b58fdc206
sorbet0404 <cyclebae@naver.com>
2026-04-02 12:03:59 +0900

    관리자가 아니면 페이지 못들어오게

--- src/test/java/com/back/domain/member/controller/ApiV1AdmMemberControllerTest.java
+++ src/test/java/com/back/domain/member/controller/ApiV1AdmMemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.back.domain.member.controller;
 
+import com.back.domain.member.entity.Member;
 import com.back.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -67,5 +68,27 @@ public class ApiV1AdmMemberControllerTest {
 //                    .andExpect(jsonPath("$[%d].title".formatted(i)).value(post.getTitle()))
 //                    .andExpect(jsonPath("$[%d].content".formatted(i)).value(post.getContent()));
 //        }
+    }
+    @Test
+    @DisplayName("회원 다건 조회, 권한이 없는 경우")
+    void t2() throws Exception {
+
+        Member actor = memberRepository.findByUsername("user1").get();
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/adm/members")
+                                .header(
+                                        "Authorization", "Bearer %s".formatted(actor.getApiKey())
+                                )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
+                .andExpect(handler().methodName("list"))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.resultCode").value("403-1"))
+                .andExpect(jsonPath("$.msg").value("권한이 없습니다."));
     }
 }
